b5519c1345182f2bb6b2520b734a042e7ce25f89
Mr.Shree <172600005+shriram7057@users.noreply.github.com>
2026-02-16 09:27:09 +0530

    Add files via upload

---
+++ NPCI GeekStreak 2026/Meeting Rooms.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canAttend(self, arr):
+        # Code Here
+        arr.sort(key=lambda x:x[0])
+        for i in range(1,len(arr)):
+            if arr[i][0]<arr[i-1][1]:
+                return False
+        return True
